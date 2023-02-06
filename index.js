9ce777dc9fd4d6426432065e8d1bd79254af6555
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-06 04:29:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftPos = dodger.style.left.replace("px", "");
+  let left = parseInt(leftPos, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+    let leftPos = dodger.style.left.replace("px", "");
+    let left = parseInt(leftPos, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
